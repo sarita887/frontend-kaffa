6ed863fea90dd4be6d912f64d3da0b3d330a3ce3
sara valencia <saravalencia1061@gmail.com>
2026-04-07 22:27:09 -0500

    pagina principal en React

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -6,4 +5,5 @@ export default {
   },
   plugins: [],
 }
+
 
